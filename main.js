2ef688ac3e08bdf5e25ee8fc615474b2e7743f6f
GARYTHEGREAT <thegreat1967@aol.com>
2018-08-07 18:45:22 -0400

    Fixed myReligion

--- main.js
+++ main.js
@@ -5,12 +5,22 @@ let myHeight = prompt ("What's your Height?")
 console.log (myHeight)
 let myCity = prompt ("What is your city of birth?")
 console.log (myCity)
+let myPartner = prompt ("What's your partner's  name?")
+console.log (myPartner)
+let myReligion = prompt ("What's your religion?")
+let myRace = prompt ("What's your Race?")
+console.log (myRace)
+
+
+
+
 if (mySex) {
  
-    console.log ("Hello my name is" + " "+ myName + ".  I am a male and I am " + myHeight + ' tall.'+ "  I was born in " +  myCity + "." ); 
-    alert ("Hello my name is" + " "+ myName + ".  I am a male and I am " + myHeight + ' tall.'+ "  I was born in " +  myCity + "." ); 
-} else {console.log("Hello my name is " + myName + ".  I am a female and I am " + myHeight + " tall."  + "  I was born in " +  myCity + ".");
-alert ("Hello my name is " + myName + ".     I am a female and I am " + myHeight + " tall."  + "  I was born in " +  myCity + ".");
+    console.log ("Hello my name is" + " "+ myName + ".  I am a male and I am " + myHeight + ' tall.'+ "  I was born in " +  myCity + "." + " " + "My partner's name is" +" " + myPartner + "." + " " + " I am a practicing" + " " + myReligion +"."); 
+    alert ("Hello my name is" + " "+ myName + "." + " " + "I am a male and I am " + myHeight + ' tall.'+ "  I was born in " +  myCity + "." + " " + "My partner's name is" +" " + myPartner +"." + " " + " I am a practicing" + " "+ myReligion +"."); 
+} else {console.log("Hello my name is " + myName + ".  I am a female and I am " + myHeight + " tall."  + "  I was born in " +  myCity + "." + " " + "My partner's name is" +" " + myPartner +"." + " " + " I am a practicing" + " " + myReligion +".");
+alert ("Hello my name is " + myName + "." + " " + "I am a female and I am " + myHeight + " tall."  + "  I was born in " +  myCity + "." + " " 
++ "My partner's name is" + " " + myPartner +"." + " " + " I am a practicing" + " "+ myReligion +".");
 } 
 
 let elName = document.getElementById('name')
@@ -26,6 +36,18 @@ console.log(elHeight);
 let elCity = document.getElementById('city')
 elCity.innerHTML = myCity;
 console.log(elCity);
+
+let elPartner = document.getElementById('partner')
+elPartner.innerHTML = myPartner;
+console.log(elPartner);
+let elReligion = document.getElementById('religion')
+elReligion.innerHTML = myReligion;
+console.log(elReligion);
+
+
+let elRace = document.getElementById('race')
+elRace.innerHTML = myRace;
+console.log(elRace);
 
 
    
